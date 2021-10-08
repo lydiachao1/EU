5305c696427fdeec1436c8e109ce2a3a56813133
BINS <70840282+472998830@users.noreply.github.com>
2021-10-08 11:51:32 +0800

    Update eu.py

--- eu.py
+++ eu.py
@@ -52,7 +52,7 @@ APIKEY = os.environ.get("APIKEY", "wMjXmBIcHcdYqO2RrsVN")
 # Telegram Bot Push https://core.telegram.org/bots/api#authorizing-your-bot
 TG_BOT_TOKEN = os.environ.get('TG_BOT_TOKEN')  # 通过 @BotFather 申请获得，示例：1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
 TG_USER_ID = os.environ.get('TG_USER_ID')  # 用户、群组或频道 ID，示例：129xxx206
-TG_API_HOST = os.environ.get('TG_USER_ID')  # 自建 API 反代地址，供网络环境无法访问时使用，网络正常则保持默认
+TG_API_HOST = os.environ.get('TG_API_HOST', 'api.telegram.org')   # 自建 API 反代地址，供网络环境无法访问时使用，网络正常则保持默认
 
 
 # Email notification
